563dfbfb74946324326681309456df1589f0c59c
Marko Milivojević <markom@gmail.com>
2020-08-28 22:14:35 -0700

    Initial version of Modern Forms Fan Prober service

---
+++ README.md
@@ -0,0 +1,27 @@
+# Minimalist Prober for Modern Forms Fans
+
+This came from discussion on
+[Home Assistant forum](https://community.home-assistant.io/t/modern-forms-smart-fans-integration/109318/36),
+where a few users of Modern Forms fans were finding their fans to stop
+responding over time. I wanted to build a simple prober service outside the HA
+codebase to see if excessive problem, or perhaps something else.
+
+Note: All API commands are from the code-base of the
+[Modern Forms integration](https://github.com/jimpastos/ha-modernforms), since
+I was unable to fund any documented API.
+
+## Usage
+
+Basic usage: 
+
+```shell
+go run mffprober.go --host={{ip_address}}
+```
+
+There are a few other flags of interest:
+
+- `interval`: How often should the prober attempt to connect to the fan. Value
+  is in seconds, and the default is 10 seconds.
+- `exit-on-error`: Leave the polling loop on first error (of any kind). If not
+  set, prober will continue attempting to talk to the fan, even if it receives
+  errors. Defaults to `true`.
---
+++ mffprober.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+)
+
+const (
+	apiTarget = "mf"
+	apiQuery  = `{ "queryDynamicShadowData" : 1 }`
+)
+
+// Config holds the configuration parameters used throughout the prober.
+type Config struct {
+	Host        string
+	Interval    time.Duration
+	ExitOnError bool
+}
+
+// FanData holds the basic information returned by the fan.
+type FanData struct {
+	ClientID        string `json:"clientID"`
+	FanOn           bool   `json:"fanOn"`
+	FanSpeed        int    `json:"fanSpeed"`
+	FanDirection    string `json:"fanDirection"`
+	Wind            bool   `json:"wind"`
+	WindSpeed       int    `json:"windSpeed"`
+	LightOn         bool   `json:"lightOn"`
+	LightBrightness int    `json:"lightBrightness"`
+}
+
+func (fd FanData) String() string {
+	var s strings.Builder
+	s.WriteString(fmt.Sprintf("ClientID: %q; ", fd.ClientID))
+	s.WriteString(fmt.Sprintf("Fan: %v (speed: %d, direction: %q); ", fd.FanOn, fd.FanSpeed, fd.FanDirection))
+	s.WriteString(fmt.Sprintf("Wind: %v (speed: %d); ", fd.Wind, fd.WindSpeed))
+	s.WriteString(fmt.Sprintf("Light: %v (brightness: %d)", fd.LightOn, fd.LightBrightness))
+
+	return s.String()
+}
+
+var cfg Config
+
+func init() {
+
+	flag.StringVar(&cfg.Host, "host", "", "Host to probe")
+	cfg.Interval = time.Duration(*flag.Int("interval", 10, "Polling interval in seconds")) * time.Second
+	flag.BoolVar(&cfg.ExitOnError, "exit-on-error", true, "Exit polling loop on error")
+
+	flag.Parse()
+}
+
+// PollFan executes HTTP POST to query the fan status, reporting error.
+func PollFan(host string) (*FanData, error) {
+	fd := new(FanData)
+
+	url := fmt.Sprintf("http://%s/%s", cfg.Host, apiTarget)
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer([]byte(apiQuery)))
+	if err != nil {
+		return nil, fmt.Errorf("error formatting request: %w", err)
+	}
+
+	hc := &http.Client{}
+	resp, err := hc.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("error polling the fan: %w", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("error reading response body: %w", err)
+	}
+
+	err = json.Unmarshal(body, &fd)
+	if err != nil {
+		return nil, fmt.Errorf("error unmarshalling JSON response: %w", err)
+	}
+
+	return fd, nil
+}
+
+func main() {
+	if cfg.Host == "" {
+		log.Fatalf("Target host must be specified.")
+	}
+
+	for {
+		fd, err := PollFan(cfg.Host)
+		if err != nil {
+			msg := fmt.Sprintf("Error reading fan information from %q: %v.", cfg.Host, err)
+			if cfg.ExitOnError {
+				log.Fatalf(msg)
+			}
+			log.Printf(msg)
+		} else {
+			log.Print(fd)
+		}
+		log.Printf("Sleeping for %v\n", cfg.Interval)
+		time.Sleep(cfg.Interval)
+	}
+}
